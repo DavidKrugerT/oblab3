84730056fb70fbb2f7ab180cd1fc1e27188235ea
David Kruger <David.TropaKruger@gmail.com>
2019-05-14 16:51:49 +0200

    updated

--- Project1/list.cpp
+++ Project1/list.cpp
@@ -1,45 +1,36 @@
 #include "list.h"
 
-linked_list::linked_list()
-{
+linked_list::linked_list() {
 	//vid skapande av en ny lista ska head och tail vara nullptr
-	head = nullptr;
-	tail = nullptr;
+	head = tail = nullptr;
+	
 }
 
-linked_list::linked_list(const linked_list & src)
-{
-	head = nullptr;
-	tail = nullptr;
+linked_list::linked_list(const linked_list & src) {
+	head = tail = nullptr;
 
 	for (size_t i = 0; i < src.size(); i++)
-	{
 		push_back(src.at(i));
-	}
 }
 
-linked_list::~linked_list()
-{
+linked_list::~linked_list() {
 	for (size_t i = 0; i < size(); i++)
 		pop_front();
 }
 
-linked_list & linked_list::operator=(const linked_list & rhs)
-{
-	for (node_t * move = rhs.head; move->next; move = move->next) this->push_back(move->value);
+linked_list & linked_list::operator=(const linked_list & rhs) {
+	if (head == rhs.head) return *this;
+	while (!is_empty()) pop_front();
+	for (node_t * move = rhs.head; move; move = move->next) this->push_back(move->value);
 	return *this;
 }
 
-linked_list & linked_list::operator +=(const linked_list & rhs)
-{
-	auto a = rhs.size();
-	for (int i = 0; i < a; i++)
-		push_back(rhs.at(i));
+linked_list & linked_list::operator +=(const linked_list & rhs) {
+	for (node_t * move = rhs.head; move; move = move->next) this->push_back(move->value);
 	return *this;
 }
 
-void linked_list::push_back(double value)
-{
+void linked_list::push_back(double value) {
 	node_t * new_tail = new node_t(value);
 	if (is_empty()) head = tail = new_tail;
 	else {
@@ -49,8 +40,7 @@ void linked_list::push_back(double value)
 	}
 }
 
-void linked_list::push_front(double value)
-{
+void linked_list::push_front(double value) {
 	node_t * new_head = new node_t(value);
 
 	if (is_empty())	head = tail = new_head;
@@ -61,25 +51,21 @@ void linked_list::push_front(double value)
 	}
 }
 
-bool linked_list::is_empty() const
-{
+bool linked_list::is_empty() const {
 	return head ? false : true;
 }
 
 void linked_list::print() const {
-	for (node_t *it = head; it; it = it->next) {
+	for (node_t *it = head; it; it = it->next) 
 		std::cout << it->value << (it->next ? ", " : "\n");
-	}
 }
 
 void linked_list::print_reverse() const {
-	for (node_t *it = tail; it; it = it->prev) {
+	for (node_t *it = tail; it; it = it->prev) 
 		std::cout << it->value << (it->prev ? ", " : "\n");
-	}
 }
 
-double linked_list::front() const
-{
+double linked_list::front() const {
 	return head ? head->value : 0;
 }
 
@@ -87,8 +73,7 @@ double linked_list::back() const {
 	return tail ? tail->value : 0;
 }
 
-double linked_list::at(size_t pos) const
-{
+double linked_list::at(size_t pos) const {
 	node_t * current_pos = head;
 	for (size_t i = 0; i < pos; i++)
 		current_pos = current_pos->next;
@@ -96,8 +81,7 @@ double linked_list::at(size_t pos) const
 	return current_pos->value;
 }
 
-size_t linked_list::size() const
-{
+size_t linked_list::size() const {
 	std::size_t size = 0;
 	for (node_t * it = this->head; it != nullptr; size++)
 		it = it->next;
@@ -106,13 +90,10 @@ size_t linked_list::size() const
 
 double linked_list::pop_front() {
 	double value = head->value;
-	if (size() == 1)
-	{
+	if (size() == 1) {
 		delete head;
 		head = nullptr;
-	}
-	else
-	{
+	} else {
 		node_t * temp = head->next;
 		temp->prev = nullptr;
 		delete head;
@@ -123,13 +104,10 @@ double linked_list::pop_front() {
 
 double linked_list::pop_back() {
 	double value = tail->value;
-	if (size() == 1)
-	{
-		delete tail; //vi tar bort det som är i tail "pekaren" sen sätter vi tail till nullptr
+	if (size() == 1) {
+		delete tail; 
 		tail = nullptr;
-	}
-	else
-	{
+	} else {
 		node_t * temp = tail->prev;
 		temp->next = nullptr;
 		delete tail;
@@ -142,15 +120,12 @@ void linked_list::remove(size_t pos)
 {
 	node_t * del_node = head;
 	for (size_t i = 0; i < pos; i++)
-	{
 		del_node = del_node->next;
-	}
 	if (!del_node->prev)
 		pop_front();
 	else if (!del_node->next)
 		pop_back();
-	else
-	{
+	else {
 		del_node->next->prev = del_node->prev;
 		del_node->prev->next = del_node->next;
 		delete del_node;
@@ -163,15 +138,12 @@ void linked_list::insert(double value, size_t pos)
 	for (size_t i = 0; i < pos; i++) ins_pos = ins_pos->next;
 	if (!ins_pos->prev) push_front(value);
 	else if (!ins_pos->next) push_back(value);
-	else
-	{
+	else {
 		node_t * ins_node = new node_t(value, ins_pos, ins_pos->prev);
 		ins_pos->prev->next = ins_node;
 		ins_pos->prev = ins_node;
 	}
 }
 
-linked_list::node_t::node_t(double value, node_t * next, node_t * prev) : prev(prev), next(next), value(value)
-{
-
-}
+linked_list::node_t::node_t(double value, node_t * next, node_t * prev)
+	: prev(prev), next(next), value(value) { }
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,44 +1,47 @@
 #include "list.h"
 
-void print_list(linked_list l);
-bool is_sorted(linked_list l);
+void print_list(const linked_list l);
+bool is_sorted(const linked_list &l);
 linked_list merge(linked_list list1, linked_list list2);
 
 int main()
 {
+
+
 	//uppgift 1
 	linked_list L1, L2, L3;
 
-	for (int i = 0; i < 5; i++)
-	{
+	for (int i = 0; i < 99; i++) {
 		L1.push_back(L1.back() + rand() % 20);
 		L2.push_back(L2.back() + rand() % 20);
 		L3.push_back(L3.back() + rand() % 20);
 	}
+	std::cout << "L1: " << std::endl;
 	L1.print();
+	std::cout << "L2: " << std::endl;
 	L2.print();
-	////Uppgift 2 och 3 
-	//if (L1.at(49) < L2.at(49))
-	//{
-	//	std::cout << "L2 has a larger element " << std::endl;
-	//	L2.remove(49);
-	//}
-	//else
-	//{
-	//	std::cout << "L1 has a larger element " << std::endl;
-	//	L1.remove(49);
-	//}
+	//Uppgift 2 och 3 
+	if (L1.at(49) < L2.at(49)) {
+		std::cout << "L2 has a larger element " << std::endl;
+		L2.remove(49);
+	} else {
+		std::cout << "L1 has a larger element " << std::endl;
+		L1.remove(49);
+	}
 
-	////Uppgift 4
-	//L3 = L1;
-	//L3.print();
+	//Uppgift 4
+	L3 = L1;
 
-	////uppgift 5
-	//for (int i = 0; i < 50; i++)
-	//{
-	//	L3.pop_back();
-	//	L3.push_front(L3.pop_back());
-	//}
+	L3.print();
+
+	//uppgift 5
+	for (int i = 0; i < 49; i++) {
+		L3.pop_back();
+		L3.push_front(L3.pop_back());
+	}
+	std::cout << "pop back varannan L3: " << std::endl;
+
+	print_list(L3);
 
 	//uppgift 7
 	linked_list L4 = merge(L1, L2);
@@ -56,19 +59,17 @@ int main()
 	return 0;
 }
 
-linked_list merge(linked_list list1, linked_list list2) 
-{
-
+linked_list merge(linked_list list1, linked_list list2) {
 	linked_list new_list;
 
-	while (!list1.is_empty() && list2.is_empty())
-		list1.front() > list2.front() ? new_list.push_back(list2.pop_front()) : new_list.push_back(list2.pop_front());
+	while (!list1.is_empty() && !list2.is_empty())
+		list1.front() > list2.front() ? new_list.push_back(list2.pop_front()) : new_list.push_back(list1.pop_front());
 	while (!list1.is_empty()) new_list.push_back(list1.pop_front());
 	while (!list2.is_empty()) new_list.push_back(list2.pop_front());
 	return new_list;
 }
 
-bool is_sorted(linked_list l) {
+bool is_sorted(const linked_list &l) {
 	for (int i = 0; i < (l.size() - 1); i++)
 		if (l.at(i) > l.at(i + 1))
 			return false;
@@ -77,7 +78,6 @@ bool is_sorted(linked_list l) {
 }
 
 //uppgift 6 
-void print_list(linked_list l)
-{
+void print_list(const linked_list l) {
 	l.print();
 }
